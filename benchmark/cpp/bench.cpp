3faa6b38f4d63c3f1b6d6f4c6680a4ebfa37b578
Konstantin Makarchev <kostya27@gmail.com>
2014-06-16 21:08:11 +0400

    fix

--- benchmark/cpp/bench.cpp
+++ benchmark/cpp/bench.cpp
@@ -7,8 +7,6 @@ using namespace std;
 
 string str = "GET / HTTP/1.1\nHost: www.example.com\nConnection: keep-alive\nUser-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_3) AppleWebKit/535.11 (KHTML, like Gecko) Chrome/17.0.963.78 S\nAccept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8\nAccept-Encoding: gzip,deflate,sdch\nAccept-Language: en-US,en;q=0.8\nAccept-Charset: ISO-8859-1,utf-8;q=0.7,*;q=0.3\n\n";
 
-map<string, string> headers;
-
 class Parser {
 public:
   map<string, string> headers;
