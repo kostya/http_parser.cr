6cca1f130a48463839c1c75fe96e08fbb84334da
Konstantin Makarchev <kostya27@gmail.com>
2014-07-12 21:27:41 +0400

    fix

--- spec/request_spec.cr
+++ spec/request_spec.cr
@@ -13,14 +13,13 @@ Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8
 Accept-Encoding: gzip,deflate,sdch
 Accept-Language: en-US,en;q=0.8
 Accept-Charset: ISO-8859-1,utf-8;q=0.7,*;q=0.3
-Content-Lenght: 3
+Content-Length: 3
 
-aaa
-"
+aaa"
     parser << str
     parser.http_version.should eq("1.1")
     parser.done.should be_true
-    parser.headers.should eq({"Host" => "www.example.com", "Connection" => "keep-alive", "User-Agent" => "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_3) AppleWebKit/535.11 (KHTML, like Gecko) Chrome/17.0.963.78 S", "Accept" => "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8", "Accept-Encoding" => "gzip,deflate,sdch", "Accept-Language" => "en-US,en;q=0.8", "Accept-Charset" => "ISO-8859-1,utf-8;q=0.7,*;q=0.3", "Content-Lenght" => "3"})
+    parser.headers.should eq({"Host" => "www.example.com", "Connection" => "keep-alive", "User-Agent" => "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_3) AppleWebKit/535.11 (KHTML, like Gecko) Chrome/17.0.963.78 S", "Accept" => "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8", "Accept-Encoding" => "gzip,deflate,sdch", "Accept-Language" => "en-US,en;q=0.8", "Accept-Charset" => "ISO-8859-1,utf-8;q=0.7,*;q=0.3", "Content-Length" => "3"})
     parser.method.should eq("POST")
     parser.body.should eq("aaa")
     parser.request_url.should eq("/")
