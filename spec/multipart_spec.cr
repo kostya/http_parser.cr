0ac495b1ea5566345e505bbb1e30032c96466787
Konstantin Makarchev <kostya27@gmail.com>
2016-12-23 01:43:31 +0300

    formatter

--- spec/multipart_spec.cr
+++ spec/multipart_spec.cr
@@ -3,15 +3,15 @@ require "./spec_helper"
 describe "HttpParser::Multipart" do
   it "rfc1867" do
     str = "--AaB03x\r\n" +
-      "content-disposition: form-data; name=\"field1\"\r\n" +
-      "\r\n" +
-      "Joe Blow\r\nalmost tricked you!\r\n" +
-      "--AaB03x\r\n" +
-      "content-disposition: form-data; name=\"pics\"; filename=\"file1.txt\"\r\n" +
-      "Content-Type: text/plain\r\n" +
-      "\r\n" +
-      "... contents of file1.txt ...\r\r\n" +
-      "--AaB03x--\r\n"
+          "content-disposition: form-data; name=\"field1\"\r\n" +
+          "\r\n" +
+          "Joe Blow\r\nalmost tricked you!\r\n" +
+          "--AaB03x\r\n" +
+          "content-disposition: form-data; name=\"pics\"; filename=\"file1.txt\"\r\n" +
+          "Content-Type: text/plain\r\n" +
+          "\r\n" +
+          "... contents of file1.txt ...\r\r\n" +
+          "--AaB03x--\r\n"
 
     m = HttpParser::Multipart.new("AaB03x")
     m << str
@@ -54,15 +54,15 @@ describe "HttpParser::Multipart" do
 
   it "noTrailing\\r\\n" do
     str = "--AaB03x\r\n" +
-      "content-disposition: form-data; name=\"field1\"\r\n" +
-      "\r\n" +
-      "Joe Blow\r\nalmost tricked you!\r\n" +
-      "--AaB03x\r\n" +
-      "content-disposition: form-data; name=\"pics\"; filename=\"file1.txt\"\r\n" +
-      "Content-Type: text/plain\r\n" +
-      "\r\n" +
-      "... contents of file1.txt ...\r\r\n" +
-      "--AaB03x--"
+          "content-disposition: form-data; name=\"field1\"\r\n" +
+          "\r\n" +
+          "Joe Blow\r\nalmost tricked you!\r\n" +
+          "--AaB03x\r\n" +
+          "content-disposition: form-data; name=\"pics\"; filename=\"file1.txt\"\r\n" +
+          "Content-Type: text/plain\r\n" +
+          "\r\n" +
+          "... contents of file1.txt ...\r\r\n" +
+          "--AaB03x--"
 
     m = HttpParser::Multipart.new("AaB03x")
     m << str
@@ -77,16 +77,16 @@ describe "HttpParser::Multipart" do
 
   it "wrong boundary" do
     str = "--AaB0--------3x\r\n" +
-      "content-disposition: form-data; name=\"field1\"\r\n" +
-      ": foo\r\n" +
-      "\r\n" +
-      "Joe Blow\r\nalmost tricked you!\r\n" +
-      "--AaB03x\r\n" +
-      "content-disposition: form-data; name=\"pics\"; filename=\"file1.txt\"\r\n" +
-      "Content-Type: text/plain\r\n" +
-      "\r\n" +
-      "... contents of file1.txt ...\r\r\n" +
-      "--AaB03x--"
+          "content-disposition: form-data; name=\"field1\"\r\n" +
+          ": foo\r\n" +
+          "\r\n" +
+          "Joe Blow\r\nalmost tricked you!\r\n" +
+          "--AaB03x\r\n" +
+          "content-disposition: form-data; name=\"pics\"; filename=\"file1.txt\"\r\n" +
+          "Content-Type: text/plain\r\n" +
+          "\r\n" +
+          "... contents of file1.txt ...\r\r\n" +
+          "--AaB03x--"
 
     m = HttpParser::Multipart.new("AaB03x")
     expect_raises do
